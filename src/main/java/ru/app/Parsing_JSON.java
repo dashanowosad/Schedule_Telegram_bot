e240a5c5b95c324fde13962b6bb8004cf3997897
Dasha <dashanowosad@yandex.ru>
2021-10-05 22:54:29 +0700

    Simplification JSON

--- src/main/java/ru/app/Parsing_JSON.java
+++ src/main/java/ru/app/Parsing_JSON.java
@@ -1,5 +1,7 @@
 package ru.app;
 
+import com.pengrad.telegrambot.model.Update;
+
 import java.io.IOException;
 
 public class Parsing_JSON {
@@ -7,15 +9,10 @@ public class Parsing_JSON {
     private String UserId; //id пользователя
     private String Language; //язык пользователя
 
-    public Parsing_JSON(String JSON) throws IOException {
-        this.Text = JSON.substring(JSON.indexOf("text="), JSON.indexOf(", entities"))
-                .replace("text='","")
-                .replace("'","");
-        this.UserId = JSON.substring(JSON.indexOf("User{id="), JSON.indexOf(", is_bot"))
-                .replace("User{id=","");
-        this.Language = JSON.substring(JSON.indexOf("language_code='"), JSON.indexOf(", can_join_groups"))
-                .replace("language_code='","")
-                .replace("'","");
+    public Parsing_JSON(Update JSON) throws IOException {
+        this.Text = JSON.message().text();
+        this.UserId = JSON.message().from().id().toString();
+        this.Language = JSON.message().from().languageCode();
     }
 
     public String GetText(){
